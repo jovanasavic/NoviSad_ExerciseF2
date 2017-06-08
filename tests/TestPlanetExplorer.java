b1080180a2b1c3944b52434a93bb16f14ecaee66
somename <someemail>
2017-06-08 20:17:07 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -14,5 +14,16 @@ public class TestPlanetExplorer {
 		
 	}
 	
+	@Test
+	public void test_MovingBack(){
+		
+		PlanetExplorer planeta = new PlanetExplorer(3,3);
+		planeta.setxExplorer(0);
+		planeta.setyExplorer(1);
+		String result = planeta.executeCommand("b");
+		
+		assertEquals("Not moving back", "(0,0,N)", result);
+	}
+	
 
 }
