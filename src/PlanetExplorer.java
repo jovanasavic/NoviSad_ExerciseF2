d5c991917b3b60981c415bc656a09a520094dbcb
somename <someemail>
2017-06-08 20:04:41 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,11 @@
 // Student ID:
 
 public class PlanetExplorer {
+	
+	int x=0;
+	int y=0;
+	String obstacles;
+	char [] position = {'N','S', 'E','W'};
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,6 +17,15 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		this.x=x;
+		this.y=y;
+		this.obstacles=obstacles;
+	}
+	public PlanetExplorer(int x, int y)
+	{
+		this.x=x;
+		this.y=y;
+		
 	}
 	
 	public String executeCommand(String command){
@@ -25,7 +39,39 @@ public class PlanetExplorer {
 		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
+		String position;
+		if (command.equals("f"))
+		{
+			this.x++;
+			position = "(" + this.getObstacles() + "," + this.getY() + "," + this.getPosition(0) + ")";
+		}
+		
+		
 		
 		return null;
+	}
+	public char getPosition(int number) {
+		return position[number];
+	}
+	public void setPosition(char[] position) {
+		this.position = position;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public String getObstacles() {
+		return obstacles;
+	}
+	public void setObstacles(String obstacles) {
+		this.obstacles = obstacles;
 	}
 }
